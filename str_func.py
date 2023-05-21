09b7497084fc3f40a72e41009302242282e42fa1
korchaginks <kkonstantinserg@gmail.com>
2023-05-21 21:51:06 +0300

    create new func in str_func.py

--- str_func.py
+++ str_func.py
@@ -1,5 +1,19 @@
-def title_charms(text: str):
+def title_letters(text: str):
     """
     Функция превращает все строчные буквы входящей строки в заглавные
     """
     return text.upper()
+
+
+def title_words(text: str):
+    """
+Возвращает строку, где все слова начинаются с заглавной буквы
+:param text: str
+:return: str
+    """
+    words = text.split()
+    title_words_list = []
+    for word in words:
+        title_words_list.append(word.title())
+
+    return ' '.join(title_words_list)
